4f1e425a778ec2d0fad4d65617b2f4ba0fa56417
B.J. Ray <bjandraney@mac.com>
2011-02-20 21:03:36 -0600

    added basic product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Product.delete_all
+Product.create(:title => 'Faucet 1',
+  :description => 
+    %{<p>
+        Glacier Bay Builders 4 in. 2-Handle Low-Arc Bathroom Faucet in Brushed Nickel.
+      </p>},
+  :image_url =>   '/images/faucet1.jpg',    
+  :price => 34.00,
+  :est_install_time_mins => 15)
+# . . .
+Product.create(:title => 'Faucet 2',
+  :description =>
+    %{<p>
+        Glacier Bay Constructor 4 in. 2-Handle Low-Arc Bathroom Faucet in Chrome.
+      </p>},
+  :image_url => '/images/faucet2.jpg',
+  :price => 24.86,
+  :est_install_time_mins => 15)
+# . . .
+Product.create(:title => 'Faucet 3',
+  :description =>
+    %{<p>
+        Price Pfister Pasadena 8 in. 2-Handle High-Arc Bathroom Faucet in Brushed Nickel.
+      </p>},
+  :image_url => '/images/faucet3.jpg',
+  :price => 98.00,
+  :est_install_time_mins => 30)
---
+++ spec/views/products/show.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "products/show.html.erb" do
+  before(:each) do
+    @product = assign(:product, stub_model(Product,
+      :title => "Title",
+      :description => "MyText",
+      :image_url => "Image Url",
+      :price => "9.99",
+      :est_install_time_mins => 1
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Title/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/MyText/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Image Url/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/9.99/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/1/)
+  end
+end
